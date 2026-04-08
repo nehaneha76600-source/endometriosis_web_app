e574e6a4b8d79edce0236fb4f8ae6ee6a1eb373e
nehavenkadesan <nehav22@karunya.edu.in>
2026-04-08 14:33:22 +0530

    ready for railway deployment

--- app.py
+++ app.py
@@ -365,5 +365,5 @@ def logout():
 
 if __name__ == "__main__":
     init_db()
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port, debug=True)
